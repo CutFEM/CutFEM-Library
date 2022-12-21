b8a94f1a48d6b1ae6e6c9fcb84eefa8561b72d16
Thomasfra <Thomasfra@users.noreply.github.com>
2022-12-21 10:44:12 +0100

    start testing quadrature

--- cpp/test/test_quadrature.hpp
+++ cpp/test/test_quadrature.hpp
@@ -6,32 +6,38 @@ TEST_CASE("Test quadrature Lobatto 1D", "[Quadrature Lobatto]") {
    for (int order = 1; order <= 7; ++order) {
 
       const auto *lobatto = Lobatto(order);
+      double val          = 0.;
       for (int i = 0; i < lobatto->getNbrOfQuads(); ++i) {
-         //  REQUIRE(std::fabs(qf.p(i)[0] - ip[i][0]) < Epsilon);
-         //  REQUIRE(std::fabs(qf.w(i) - w[i]) < Epsilon);
+         const auto &ip(lobatto->at(i));
+         val += ip.a * pow(ip.x, order);
       }
+      REQUIRE(std::fabs(val - 1. / (order + 1.)) < 10 * Epsilon);
    }
 }
 
-// TEST_CASE("Test quadrature Legendre 1D", "[Quadrature]")
-// {
+TEST_CASE("Test quadrature Gauss-Legendre1D", "[Quadrature Gauss-Legendre]") {
+   for (int order = 1; order <= 17; ++order) {
 
-//     for (int order = 1; order < 15; ++order)
-//     {
-//         const QuadratureFormular<1> qf(order);
-//         const auto &ip(qf.p());
-//         const auto &w(qf.w());
-//         REQUIRE(ip.size() == qf.size());
-//         for (int i = 0; i < ip.size(); ++i)
-//         {
-//             REQUIRE(std::fabs(qf.p(i)[0] - ip[i][0]) < Epsilon);
-//             REQUIRE(std::fabs(qf.w(i) - w[i]) < Epsilon);
-//         }
-//         Real val = 0.;
-//         for (int i = 0; i < ip.size(); ++i)
-//         {
-//             val += w[i] * pow(ip[i][0], order);
-//         }
-//         REQUIRE(std::fabs(val - 1. / (order + 1.)) < 10*Epsilon);
-//     }
+      const auto *qf = QF_Simplex<R1>(order);
+      double val     = 0.;
+      for (int i = 0; i < qf->getNbrOfQuads(); ++i) {
+         const auto &ip(qf->at(i));
+         val += ip.a * pow(ip.x, order);
+      }
+      REQUIRE(std::fabs(val - 1. / (order + 1.)) < 10 * Epsilon);
+   }
+}
+
+// TEST_CASE("Test quadrature Gauss-Legendre 2D for quad",
+//           "[Quadrature Gauss-Legendre 2D]") {
+//    for (int order = 1; order <= 17; ++order) {
+
+//       const auto *qf = QF_Simplex<R1>(order);
+//       double val     = 0.;
+//       for (int i = 0; i < qf->getNbrOfQuads(); ++i) {
+//          const auto &ip(qf->at(i));
+//          val += ip.a * pow(ip.x, order);
+//       }
+//       REQUIRE(std::fabs(val - 1. / (order + 1.)) < 10 * Epsilon);
+//    }
 // }
